843b9a4cd2b9c495959d452b2cf227b1babfcdc9
dhilshad123 <97964030+dhilshad123@users.noreply.github.com>
2022-01-25 09:30:34 -0500

    Update Student.java

--- src/studentlist1/Student.java
+++ src/studentlist1/Student.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * name: dhilshad
+ * Date:25 jan
  */
 package studentlist;
 
