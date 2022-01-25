d12ea3a0ac0a6db88cfc3e5ba468dc1e4f501a3b
dhilshad123 <dilshadyouseph944@gmail.com>
2022-01-25 10:06:34 -0500

    add part time student class

---
+++ src/studentlist1/part_time_student.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentlist1;
+
+/**
+ *
+ * @author dhils
+ */
+public class part_time_student extends Student {
+    private int numCourse;
+
+    public int getNumCourse() {
+        return numCourse;
+    }
+
+    public void setNumCourse(int numCourse) {
+        this.numCourse = numCourse;
+    }
+    
+    
+    
+    
+}
